31563c3ea7dc3d8afb9803a12e4332c1050f9388
OsagieGuobadia <119604873+OsagieGuobadia@users.noreply.github.com>
2022-12-01 15:55:20 +0100

    Add files via upload

---
+++ Node.js
@@ -0,0 +1,58 @@
+<!-- NODE JS -->
+
+//     <!-- 1. Node Js is considered to be a single thread application compared to other 
+//     backend programming languages because it is performed synchrounously on a single thread ie. runs by using a component or element 
+//     one at a time whereby information or data is processes faster and easier,
+//     Node Js has an advantage over other backend programming languages based on speed. -->
+
+//     <!-- 2. To initialize a package.json in an empty file.. all you have to type in the terminal is :
+//         "npm init -y"
+//        this creates a package with default settings or data. -->
+
+//        <!-- 3. Nodemon is needed in the backend or node Js because it allows us to restart the server or node environment without having to 
+//        do anything making coding easier, it will automatically restart the server once there is a change made.  -->
+
+//        <!-- 4. import mysql from "mysql"
+
+//             const  mysql = require('mysql');
+//             const  express = require('express');
+//             const  app= express()
+//             const PORT= 3000// defines the Port of the host.
+
+//           const db = mysql.createconnection({
+//                 host: "localhost"
+//                 user:  "root"
+//                 password: "password"
+//                 database: "DB_NAME"
+//           }) -->
+
+//           <!-- db.connect((err) =>{
+//            if(err){
+//             console.log(err.message)
+//           }else{
+//             console.log("Database connected successfully")
+//           }
+//           });
+//           app.listen(PORT); -->
+
+//        <!-- 5.B -->
+        
+//        <!-- 6.B -->
+
+//        <!-- 7. GET, POST, PUT, PATCH, DELETE
+
+//        GET- this is used to retreive data from a server, it is also a non secured way of viewing data from a server.
+
+//        POST- this is used to create data to a server in a secured way whereby the credentials of the data cant be viewed.
+
+//        PUT- this is used to modify a data in a server.
+
+//        PATCH- this is used to update data on a server.
+
+//        DELETE- THis is used to deleted or remove data from a server. -->
+
+//        <!-- 8. Const fs = require('fs');
+//        fs.copyFile('the folder you must you want to copy from', 'the folderyou want to copy the information to', (err)=>{
+//         if (err) throw err;
+//         console.log('File was copied to destination');
+//        }); -->
